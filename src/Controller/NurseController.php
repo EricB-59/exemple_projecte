9e74f5dc1dc08177fbf2ad4e5436992bd32c71f0
EricB-59 <151823587+EricB-59@users.noreply.github.com>
2024-10-01 16:06:50 +0200

    eric

--- src/Controller/NurseController.php
+++ src/Controller/NurseController.php
@@ -2,16 +2,34 @@
 
 namespace App\Controller;
 
+use LengthException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class NurseController extends AbstractController
 {
     #[Route('/nurse', name: 'app_nurse')]
-    public function index(): JsonResponse
-    {
-        $nurses = json_decode(file_get_contents('DATA.json', '..'), true);
-        return $this->json($nurses);
+    public function index(): Response {   
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $username = $_POST['username'];
+            //$passwd = $_POST['passwd'];
+            //echo $username;
+        }
+        //$username = $_POST['username'];
+        //$passwd = $_POST['passwd'];
+        $json_data = file_get_contents('/home/ericb/Projects/exemple_projecte/src/Controller/DATA.json');
+        $data_array = json_decode($json_data, true);
+        for ($i=0; $i < count($data_array); $i++) { 
+            foreach ($data_array[$i] as $desc => $value){
+                if ($desc == "first_name" && $value == "si") {
+                    if($data_array[$i]["password"] == "si"){
+                        return new Response("SI");
+                    };
+                }
+            }
+        };
+        return new Response($value);
     }
 }
