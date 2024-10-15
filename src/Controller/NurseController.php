6bf764207ca3842e3bf108e4d8ffb14e5040c15d
EricB-59 <151823587+EricB-59@users.noreply.github.com>
2024-10-15 16:01:35 +0200

    update

--- src/Controller/NurseController.php
+++ src/Controller/NurseController.php
@@ -7,32 +7,29 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
- 
+
+#[Route('/nurse', name: 'app_nurse')]
 class NurseController extends AbstractController
 {
-    #[Route('/nurse', name: 'app_nurse')]
-    public function index(Request $request): Response {
-        if ($request->isMethod('POST')) {
-            $firstName = $request->request->get('first_name');
-            $password = $request->request->get('password');
-           
-            $json_data = file_get_contents('/home/ericb/Projects/exemple_projecte/src/Controller/DATA.json');
-            $data_array = json_decode($json_data, true);
- 
-            for ($i = 0; $i < count($data_array); $i++) {
-                foreach ($data_array[$i] as $desc => $value) {
-                    if ($desc == "first_name" && $value == $firstName) {
-                        if ($data_array[$i]["password"] == $password) {
-                            return new Response(true);
-                        }
+    // Validación de login de un enfermero
+    #[Route('/login', methods: ['POST'], name: 'app_nurse_login')]
+    public function nurseLogin(Request $request): JsonResponse
+    {
+        $firstName = $request->request->get('first_name');
+        $password = $request->request->get('password');
+
+        $json_data = file_get_contents('DATA.json');
+        $data_array = json_decode($json_data, true);
+
+        for ($i = 0; $i < count($data_array); ++$i) {
+            foreach ($data_array[$i] as $desc => $value) {
+                if ('first_name' == $desc && $value == $firstName) {
+                    if ($data_array[$i]['password'] == $password) {
+                        return $this->json(true, Response::HTTP_OK);
                     }
                 }
             }
- 
-            return new Response(content: false);
         }
- 
-        // Si la petición no es POST, devolver un mensaje o una vista
-        return new Response('Por favor, envía los datos mediante un formulario POST.');
+        return $this->json(false, Response::HTTP_NOT_FOUND);
     }
 }
